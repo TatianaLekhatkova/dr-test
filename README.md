263e0dd92491a6d347fb0f9f3c51e38e85eec648
Tatiana Lekhatkova <tlekhatkova@gmail.com>
2022-10-25 16:45:59 +0200

    tatiana test and comments

--- README.md
+++ README.md
@@ -27,3 +27,17 @@ You are not supposed to work hard to make this work. This should be easy. If it 
 
 If the work is done and was easy. Please share your code with us the way you want. You are supposed to know git, so don't send us a ZIP of your code ;)
 
+# RESPONSE
+## What is wrong with this repo
+- No error handling, what is really crucial. There are many ways to deal with them: in Vyte we used callbacks and logged errors in console, In Nestor we used async-await with try catch blocks. And errors were logged in Datadog by winston logger. Express has a built-in error handler, but allows to write custom ones.
+
+- No path-aliases. I prefer to use aliases like "@routes" instead of "../routes", aliases are more readable and safe.
+
+- Exposing hard-coded PORT. Normally such things are defined in constants or rather in enviroment variables. In Nestor we used .env file to stock such data.
+
+- It feels like there are also some security problems, but I cannot figure out what exactly.
+
+- No tests. For such a small app I would use mocha-chai, for more complex app - jest.
+
+
+
--- controllers/content.js
+++ controllers/content.js
@@ -1,5 +1,6 @@
 
-import { Content } from '../models/content.js'
+import { Content } from '../models/content.js';
+import fetch from 'node-fetch';
 
 // Get
 export const getContent = async (req, res) => {
@@ -7,6 +8,8 @@ export const getContent = async (req, res) => {
     const contents = await Content.findAll()
     res.send(contents)
 }
+
+
 
 /**
  * This function seems useless but is useful for this company
@@ -25,6 +28,24 @@ async function getDummyData() {
  * https://jsonplaceholder.typicode.com/todos/1
  * This method should only return title and completed
  */
-async function getWebContent() {
-    throw new Error('not implemented yet')
+export const getWebContent = async (req, res) => {
+    let result = {}
+    try {
+        const data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        try {
+            for await (const chunk of data.body) {
+                result = {
+                    title: JSON.parse(chunk.toString()).title,
+                    completed: JSON.parse(chunk.toString()).completed
+                }
+            }
+        } catch (err) {
+            console.error("Error in the reading chunks of response", err);
+        }
+        
+        res.send(result)
+    } catch (error) {
+        console.log("Ooops, smth wrong in the controller getWebContent", error)
+    }
+
 }
